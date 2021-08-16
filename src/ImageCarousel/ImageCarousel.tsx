1d9b73b749e45bc9c1177acb6895ca7cb5ae5863
tbrittain <treybrittain@gmail.com>
2021-08-15 21:37:48 -0500

    Completed about section

---
+++ src/ImageCarousel/ImageCarousel.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import './ImageCarousel.css'
+
+interface ImageDetails {
+  img: string,
+  alt: string,
+  label?: string
+}
+
+export type ImageCarouselProps = ImageDetails[]
+
+function ImageCarousel (props: { images: ImageCarouselProps }) {
+  return (
+    <Carousel
+      autoPlay={true}
+      infiniteLoop={true}
+      interval={7000}
+      showThumbs={false}
+      centerMode={true}
+      showIndicators={false}
+      showStatus={false}
+    >
+      {
+        props.images.map(image => (
+          <div
+            className='carousel-container'
+          >
+            <img
+              src={image.img}
+              alt={image.alt}
+            />
+            {
+              image.label &&
+              <p
+                className='image-label'
+              >
+                {image.label}
+              </p>
+            }
+          </div>
+        ))
+      }
+    </Carousel>
+  )
+}
+
+export default ImageCarousel
