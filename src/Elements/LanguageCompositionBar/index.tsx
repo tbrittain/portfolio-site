f7bc16787f9d9ddfd60ea73218d7defd203a8eac
tbrittain <treybrittain@gmail.com>
2022-03-31 23:34:59 -0500

    Progress on language plot

--- src/Elements/LanguageCompositionBar/index.tsx
+++ src/Elements/LanguageCompositionBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './LanguageCompositionBar.scss'
 import axios from 'axios'
 import Spinner from '../Spinner'
@@ -6,19 +6,37 @@ import Spinner from '../Spinner'
 const BASE_URL =
   'https://pastime-delicious-maximum.netlify.app/.netlify/functions/line-count'
 
+interface LanguageResult {
+  filetype: {
+    name: string
+    color: string
+  }
+  num_lines: number
+  size: number
+}
+
 function LanguageCompositionBar(props): JSX.Element {
   const gitRepo: string = props.gitRepo
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const totalNumberOfLines = useRef(0)
 
   useEffect(() => {
     axios
-      .post(BASE_URL, {
-        repoUrl: gitRepo,
+      .get(BASE_URL, {
+        params: {
+          url: gitRepo,
+        },
       })
       .then((res) => {
-        setData(res.data)
+        const result = res.data as LanguageResult[]
+        setData(result)
+        // reduce each language to the number of lines
+        totalNumberOfLines.current = result.reduce(
+          (acc, curr) => acc + curr.num_lines,
+          0
+        )
         setLoading(false)
       })
       .catch((err) => {
@@ -33,8 +51,11 @@ function LanguageCompositionBar(props): JSX.Element {
 
   console.log(data)
   console.log(error)
+  console.log(totalNumberOfLines.current)
 
-  return <>gitRepo: {gitRepo}</>
+  return (
+    <>language goes here</>
+  )
 }
 
 export default LanguageCompositionBar
